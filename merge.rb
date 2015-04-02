e1bca42cd21020f91d4d6ce8697a07a6b402da48
soccerKevin <soccerkevint11@gmail.com>
2015-04-02 16:00:42 -0700

    Benchmarked ruby.sort method too

--- merge.rb
+++ merge.rb
@@ -4,15 +4,27 @@ require 'benchmark'
 include Benchmark
 
 long = LONG
-# puts long.shuffle!.to_s
+puts long.shuffle!.to_s
+puts long.merge_sort!.to_s
 
 n = 100
 Benchmark.benchmark(CAPTION, 4, FORMAT, "total: ", "avg: ") do |x|
   total = x.report do 
       n.times do
-        l1 = long
+        l1 = Array.new(long)
         l1.merge_sort!
       end
     end
   [total, total/n]
 end
+
+Benchmark.benchmark(CAPTION, 4, FORMAT, "total: ", "avg: ") do |x|
+  total = x.report do 
+      n.times do
+        l1 = Array.new(long)
+        l1.sort
+      end
+    end
+  [total, total/n]
+end
+
